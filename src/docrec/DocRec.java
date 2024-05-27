d3c011173928ec26d1772488f626e89dda6de981
ItsDMAF <dmaf.andinofiallos@gmail.com>
2024-05-27 11:57:24 +0200

    First commit

---
+++ src/docrec/DocRec.java
@@ -0,0 +1,50 @@
+package docrec;
+
+public class DocRec {
+
+    public static void main(String[] args) {
+        /*
+        Potencia y proposito general:
+        Ayudar a los doctores a mantener un record virutal 
+        de la informacion de sus pacientes para poder accender y 
+        mantener informacion con un mejor nivel de organicacion.
+        
+        Mi prioridad es ayudar a la gente,ayudando a los doctores
+        mantener al dia la informacion de los pacientes 
+        */ 
+        
+        
+      /*
+        -Nombre
+        -Edad
+        -Sexo
+        -Lugar de Residencia
+        -Telefono
+        -Sintomas Principales (Por que causa visita)
+        Historia de la enfermedad actual (Describe sus
+        sintomas, desde cuando, por ejemplo: como es el dolor
+        si se irradia a otros lado, si tiene atenuantes o 
+        exacervantes etc.)
+        -Si hay otros sintomas
+        -Antecedentes personales patologicos (o se enfermedades
+        de las que padece, desde cuanfo y si toma medicamentos y cuales)
+        -Antecedentes  familiares (  si  sus padres  , hnos   o abuelos 
+        padecen de alguna enfermedad como diabetes  hipertension  
+        cáncer etc.  )
+        -Antecedentes alergicos(  si el paciente padece   de alergia 
+        a algún medicamento o alimento)
+        -Antecedentes  Hospitalarios y quirúrgicos  ( si     
+        ha estafó hospitalizado   y porque , si lo han 
+        operado   cuando y de que)
+        - Se le toman sus  signos vitales (   presion arterial , 
+        frecuencia cardiaca , pulso   ,, saturacion  de oxígeno, 
+        peso  , talla,   )
+        - Su examen fisico (se revisa al px)
+        -Diagnostico
+        - Y de acuerdo al diagnostico se le indican examenes de 
+        lab o y otros tratamientos famacologicos
+        
+        */  
+    }
+    
+}
---
+++ src/docrec/Paciente.java
@@ -0,0 +1,12 @@
+package docrec;
+
+public class Paciente {
+    private String nombtr;
+    private int edad;
+    private char sexo;
+    private String lugarResidencia;
+    private String Telefono;
+    
+    
+    
+}
